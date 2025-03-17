9ffab527661d3c3e4e6ed93a4c99f500c565ab97
Mykhailo Alipa <6442572+strobil@users.noreply.github.com>
2025-03-17 22:01:39 +0000

    feat: remove Buf CLI feature and associated files

--- test/buf-cli/latest.sh
+++ test/buf-cli/latest.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+# This test file will be executed against a scenario devcontainer.json that
+# includes the 'buf-cli' Feature with version='latest'.
+#
+# For more information, see: https://github.com/devcontainers/cli/blob/main/docs/features/test.md
+#
+# This scenario tests that the latest version of buf-cli is installed correctly.
+
 set -e
 
 # Optional: Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
-# Feature name
-check "buf command exists" command -v buf
+# Feature-specific tests
+check "buf command exists" which buf
 check "buf version" buf --version
 
 # Report result
--- test/buf-cli/specific-version.sh
+++ test/buf-cli/specific-version.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+# This test file will be executed against a scenario devcontainer.json that
+# includes the 'buf-cli' Feature with a specific version.
+#
+# For more information, see: https://github.com/devcontainers/cli/blob/main/docs/features/test.md
+#
+# This scenario tests that a specific version of buf-cli is installed correctly.
+
 set -e
 
 # Optional: Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
-# Feature name
-check "buf command exists" command -v buf
-check "buf version" buf --version | grep "1.20.0"
+# Feature-specific tests
+check "buf command exists" which buf
+check "buf specific version" buf --version | grep "1.29.0"
 
 # Report result
 reportResults
